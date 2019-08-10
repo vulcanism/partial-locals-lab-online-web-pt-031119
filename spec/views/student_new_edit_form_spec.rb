51da0053290ac3a059047d9ef40580ea9519e4c8
Savannah C <sgcorrell@gmail.com>
2019-08-10 16:30:23 +0000

    Automatically backed up by Learn

--- spec/views/student_new_edit_form_spec.rb
+++ spec/views/student_new_edit_form_spec.rb
@@ -50,7 +50,7 @@ RSpec.describe "create and edit form" do
 
     it "directly renders the form in a students/form partial with the form object passed through as a local variable" do
       view.lookup_context.prefixes = %w[students]
-      student = Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(25.years.ago, 18.years.ago))
+      student = Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(25.years.ago, to: 18.years.ago))
       assign(:student, student)
 
       render :template => "students/edit.html.erb"
