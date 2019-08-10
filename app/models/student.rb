3bcaf0bc46dec11f426d97c181ef839afaaddd65
Savannah C <sgcorrell@gmail.com>
2019-08-10 16:25:12 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+  
+  def self.search(search) 
+    if search.present?
+      self.where('NAME like ?', "%#{search}%")
+    else
+      self.all
+    end
+  end 
+  
 end
